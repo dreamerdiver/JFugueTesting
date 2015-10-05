4746678de6329c2512babc251646b04b1f9077c8
Mitch Gundrum <mgundrum@madisoncollege.edu>
2015-10-05 10:57:15 -0500

    Initial commit Testing JFugue API *Ode To Joy *Crazy Train

---
+++ src/HelloWorld.java
@@ -0,0 +1,29 @@
+/**
+ * Project:
+ * Class:
+ * Created by Meesh
+ * 10/5/15
+ */
+
+import org.jfugue.player.Player;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        Player player = new Player();
+        player.play("Eq Eq Fq Gq | Gq Fq Eq Dq | Cq Cq Dq Eq | Eq Dq Dh");
+        player.play("Eq Eq Fq Gq | Gq Fq Eq Dq | Cq Cq Dq Eq | Dq Cq Ch");
+    }
+}
+
+
+/*
+Ode To Joy
+player.play("Eq Eq Fq Gq | Gq Fq Eq Dq | Cq Cq Dq Eq | Eq Dq Dh");
+player.play("Eq Eq Fq Gq | Gq Fq Eq Dq | Cq Cq Dq Eq | Dq Cq Ch");
+*/
+
+/*
+Crazy Train
+player.play("G3q G3h. | A#3q A#3q F3q F3q");
+player.play("G3q G3h. | D#4q D#4q F3q F3q");
+*/
