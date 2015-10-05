dddf0e494fe06ba13bcc65b468bf629d9fc629d5
Mitch Gundrum <mgundrum@madisoncollege.edu>
2015-10-05 13:00:55 -0500

    Commit 10.4
    Added some tunes

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -10,8 +10,12 @@ import org.jfugue.player.Player;
 public class HelloWorld {
     public static void main(String[] args){
         Player player = new Player();
-        player.play("Eq Eq Fq Gq | Gq Fq Eq Dq | Cq Cq Dq Eq | Eq Dq Dh");
-        player.play("Eq Eq Fq Gq | Gq Fq Eq Dq | Cq Cq Dq Eq | Dq Cq Ch");
+
+        player.play("G3q G3h. | A#3q A#3q F3q F3q");
+        player.play("G3q G3h. | D#4q D#4q F3q F3q");
+
+        //player.play("T130 C5q D5q D#5q F5q | G5h D#5h | F5h D5h | C5w");
+        //player.play("T130 C5q D5q D#5q F5q | G5h D#5h | F5h G#5h | G5h D#5h | C5w");
     }
 }
 
@@ -27,3 +31,17 @@ Crazy Train
 player.play("G3q G3h. | A#3q A#3q F3q F3q");
 player.play("G3q G3h. | D#4q D#4q F3q F3q");
 */
+
+/*
+Call Me Maybe
+player.play("Rh G5h | B4q D5q G5q D5q");
+player.play("Rh. D5q | B4q D5q B5q G5q ");
+player.play("Rh. G5q | B5q C6q B5q G5q ");
+player.play("Rh. G5q | B5q A5q A5q G5q ");
+ */
+
+/*
+Humptey Dance
+player.play("T160 Rq E4q E4q E4q | G4h G4q G4q | A4q A4q D4q D#4q | E4h. B5q");
+player.play("T160 B5h B5q A5q | A5q B5h.");
+ */
