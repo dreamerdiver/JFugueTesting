9f2224fa4030f4341106c5a059fd5268e1199a71
Mitch Gundrum <mgundrum@madisoncollege.edu>
2015-10-05 21:06:28 -0500

    Commit 10.4.1
    Experimented with concepts:
    -Note values
    -Tempo
    -Chords
    -Chord Progressions
    -String Set Methods
    -Rhythms
    -Parsers
    Added some tunes

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,43 +5,87 @@
  * 10/5/15
  */
 
+import org.jfugue.midi.MidiParser;
+import org.jfugue.pattern.Pattern;
 import org.jfugue.player.Player;
+import org.jfugue.rhythm.Rhythm;
+import org.jfugue.theory.Chord;
+import org.jfugue.theory.ChordProgression;
+import org.jfugue.theory.Note;
+import org.staccato.StaccatoParserListener;
+
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiSystem;
+import java.io.File;
+import java.io.IOException;
+
 
 public class HelloWorld {
     public static void main(String[] args){
-        Player player = new Player();
+         Player player = new Player();
 
-        player.play("G3q G3h. | A#3q A#3q F3q F3q");
-        player.play("G3q G3h. | D#4q D#4q F3q F3q");
+        Pattern p1 = new Pattern("Eminw | Bminw | Eminw | Aminw").setVoice(0).setInstrument("Piano");
+        Pattern p2 = new Pattern("E4w | B4w | E4w | A4w").setVoice(1).setInstrument("Violin");
+        Rhythm rhythm = new Rhythm()
+                .addLayer("O...X...O...X...")
+                .addLayer("....S.......S...")
+                .addLayer(".```.`.`.```.`.`")
+                .addLayer("...............+");
 
-        //player.play("T130 C5q D5q D#5q F5q | G5h D#5h | F5h D5h | C5w");
-        //player.play("T130 C5q D5q D#5q F5q | G5h D#5h | F5h G#5h | G5h D#5h | C5w");
+        player.play(rhythm.getPattern().repeat(2), p1);
+        player.play(rhythm.getPattern().repeat(2), p1, p2);
     }
 }
 
 
 /*
-Ode To Joy
-player.play("Eq Eq Fq Gq | Gq Fq Eq Dq | Cq Cq Dq Eq | Eq Dq Dh");
-player.play("Eq Eq Fq Gq | Gq Fq Eq Dq | Cq Cq Dq Eq | Dq Cq Ch");
-*/
 
-/*
-Crazy Train
-player.play("G3q G3h. | A#3q A#3q F3q F3q");
-player.play("G3q G3h. | D#4q D#4q F3q F3q");
-*/
+Experimentation - E minor
+Pattern p1 = new Pattern("Eminw | Bminw | Eminw | Aminw").setVoice(0).setInstrument("Piano");
+Pattern p2 = new Pattern("Rq E5 D#5q E5q | F#5q E5h.| F#5q E5h E5q| G5h F#5h").setVoice(1).setInstrument("Xylophone");
+Pattern p3 = new Pattern("Rq B5q A5q G5q | A5q G5q F#5q D#5q | E5q E5q Rq G5q | F#5h E5h").setVoice(1).setInstrument("Xylophone");
+Pattern p4 = new Pattern("Ri E4i E4i E4i Ri Ri Ri Ri | E4i E4i E4i E4i F#4i F#4i F#4i F#4i ").setVoice(2).setInstrument("Acoustic_Bass");
+player.play(p1, p2, p1, p2, p1, p3, p1, p2);
+player.play(p1, p2, p4, p1, p2, p4, p1, p3, p4, p1, p2, p4);
 
-/*
+Wasn't Me
+player.play("T140 Rq E5i E5i E5i D5i D5i D5i | D5i D5i G5q E5q D5q | D5i D5i D5i D5i D5q C5q | G4w");
+player.play("T140 Rq E5i E5i E5q D5i D5i | D5q G5q E5q D5q | D5i D5i D5i D5i D5q C5q | G4h E4q F4q");
+player.play("T140 G4h C5q B4q | A4q A4q G4q F4q | E4q E4i E4i E4q E4q | D4h E4q F4q");
+player.play("T140 G4h C5q B4q | A4q A4q G4q F4q | E4i E4i E4i E4i E4q E4q | D4w");
+player.play("T140 Rq C5i C5i C5i C5i C5i C5i | D5q E5q Rq C4i C4i | C4q C5i C5i C5q C5i C5i | D5q E5q Rq C4i C4i");
+player.play("T140 Ri C4i E5i E5i E5i D5i D5q | D5q C5q Rq C4i C4i | C4i E5i E5i E5i E5i E5i E5q | G5q G5q ");
+player.play("T140 Rq E5i E5i E5i D5i D5i D5i | D5i D5i G5q E5q D5q | D5i D5i D5i D5i D5q C5q | G4w");
+player.play("T140 Rq E5i E5i E5q D5i D5i | D5q G5q E5q D5q | D5i D5i D5i D5i D5q C5q | G4h E4q F4q");
+player.play("T140 G4h C5q B4q | A4q A4q G4q F4q | E4q E4i E4i E4q E4q | D4h E4q F4q");
+player.play("T140 G4h C5q B4q | A4q A4q G4q F4q | E4i E4i E4i E4i E4q E4q | D4w");
+
+Rain
+player.play("T130 C5q D5q D#5q F5q | G5h D#5h | F5h D5h | C5w");
+player.play("T130 C5q D5q D#5q F5q | G5h D#5h | F5h G#5h | G5h D#5h | C5w");
+
+Super Mario Theme
+player.play("T160 E5i E5q E5q C5i E5q | G5h G4h ");
+player.play("T160 C5i. G4i. E4q | ");
+
+Sweet Child of Mine
+player.play("T210 I[Distortion_Guitar] D5q A5q G5q F#5q | F#6q F#5q D6q F#5q | D5q A5q G5q F#5q | F#6q F#5q D6q F#5q");
+player.play("T210 I[Distortion_Guitar] E5q A5q G5q F#5q | F#6q F#5q D6q F#5q | E5q A5q G5q F#5q | F#6q F#5q D6q F#5q");
+player.play("T210 I[Distortion_Guitar] D5q A5q G5q F#5q | F#6q F#5q D6q F#5q | D5q A5q G5q F#5q | F#6q F#5q D6q F#5q");
+player.play("T210 I[Distortion_Guitar] E5q A5q G5q F#5q | F#6q F#5q D6q F#5q | E5q A5q G5q F#5q | F#6q F#5q D6q F#5q");
+
 Call Me Maybe
 player.play("Rh G5h | B4q D5q G5q D5q");
 player.play("Rh. D5q | B4q D5q B5q G5q ");
 player.play("Rh. G5q | B5q C6q B5q G5q ");
 player.play("Rh. G5q | B5q A5q A5q G5q ");
- */
 
-/*
-Humptey Dance
-player.play("T160 Rq E4q E4q E4q | G4h G4q G4q | A4q A4q D4q D#4q | E4h. B5q");
-player.play("T160 B5h B5q A5q | A5q B5h.");
- */
+Crazy Train
+player.play("G3q G3h. | A#3q A#3q F3q F3q");
+player.play("G3q G3h. | D#4q D#4q F3q F3q");
+
+Ode To Joy
+player.play("Eq Eq Fq Gq | Gq Fq Eq Dq | Cq Cq Dq Eq | Eq  Dq Dh");
+player.play("Eq Eq Fq Gq | Gq Fq Eq Dq | Cq Cq Dq Eq | Dq Cq Ch");
+
+*/
