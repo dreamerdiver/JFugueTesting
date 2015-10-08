a49a0938264df0b36c83946189c5c0f3270d52de
Mitch Gundrum <mgundrum@madisoncollege.edu>
2015-10-08 17:15:06 -0500

    Commit 10.8
    Further experimentation with rhythms
    Concurrent notes
    Voices
    Full Songs
    Method formatting
    Added Tunes

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,35 +5,43 @@
  * 10/5/15
  */
 
-import org.jfugue.midi.MidiParser;
 import org.jfugue.pattern.Pattern;
 import org.jfugue.player.Player;
-import org.jfugue.rhythm.Rhythm;
-import org.jfugue.theory.Chord;
-import org.jfugue.theory.ChordProgression;
-import org.jfugue.theory.Note;
-import org.staccato.StaccatoParserListener;
-
-import javax.sound.midi.InvalidMidiDataException;
-import javax.sound.midi.MidiSystem;
-import java.io.File;
-import java.io.IOException;
-
 
 public class HelloWorld {
     public static void main(String[] args){
-         Player player = new Player();
+        Player player = new Player();
 
-        Pattern p1 = new Pattern("Eminw | Bminw | Eminw | Aminw").setVoice(0).setInstrument("Piano");
-        Pattern p2 = new Pattern("E4w | B4w | E4w | A4w").setVoice(1).setInstrument("Violin");
-        Rhythm rhythm = new Rhythm()
-                .addLayer("O...X...O...X...")
-                .addLayer("....S.......S...")
-                .addLayer(".```.`.`.```.`.`")
-                .addLayer("...............+");
+        Pattern p1 = new Pattern("A#4h+Rq_E#5q C#6h+Rq_E#5i_A#4i | C6h+Rq_D#5q A#5h+Rq_C#5q")
+                .setVoice(0)
+                .setTempo(125)
+                .setInstrument("Piano");
 
-        player.play(rhythm.getPattern().repeat(2), p1);
-        player.play(rhythm.getPattern().repeat(2), p1, p2);
+        Pattern p2 = new Pattern("G#4h+Rq_D#5q B#5h+Rq_D#5i_G#4i | A#5h+Rq_C5q G#5h+Rq_B#4q")
+                .setVoice(0)
+                .setTempo(125)
+                .setInstrument("Piano");
+
+        Pattern vocals = new Pattern("A#4h+Rq_B#4i_E#5i")
+                .setVoice(1)
+                .setTempo(125)
+                .setInstrument("Guitar");
+
+        Pattern p3 = new Pattern(p1, p2);
+
+        Pattern p4 = new Pattern("C#5h+Rq_G#5q E#6h+Rq_G#5i_C#5i | D#6h+Rq_E#5q C#6h+Rq_C#5q")
+                .setVoice(0)
+                .setTempo(125)
+                .setInstrument("Piano");
+
+        Pattern p5 = new Pattern("B4h+Rq_F#5q D#6h+Rq_F#5i_B4i | C#6h+Rq_D#5q B5h+Rq_B4q")
+                .setVoice(0)
+                .setTempo(125)
+                .setInstrument("Piano");
+
+        Pattern p6 = new Pattern(p4, p5);
+
+        player.play(p1.repeat(2), p3.repeat(2), p6.repeat(2));
     }
 }
 
@@ -48,6 +56,16 @@ Pattern p4 = new Pattern("Ri E4i E4i E4i Ri Ri Ri Ri | E4i E4i E4i E4i F#4i F#4i
 player.play(p1, p2, p1, p2, p1, p3, p1, p2);
 player.play(p1, p2, p4, p1, p2, p4, p1, p3, p4, p1, p2, p4);
 
+Pattern p1 = new Pattern("Eminw | Bminw | Eminw | Aminw").setVoice(0).setInstrument("Piano");
+        Pattern p2 = new Pattern("E4w | B4w | E4w | A4w").setVoice(1).setInstrument("Violin");
+        Rhythm rhythm = new Rhythm()
+                .addLayer("O...X...O...X...")
+                .addLayer("....S.......S...")
+                .addLayer(".```.`.`.```.`.`")
+                .addLayer("...............+");
+
+        player.play(rhythm.getPattern().repeat(4), p1.repeat(2), p2);v
+
 Wasn't Me
 player.play("T140 Rq E5i E5i E5i D5i D5i D5i | D5i D5i G5q E5q D5q | D5i D5i D5i D5i D5q C5q | G4w");
 player.play("T140 Rq E5i E5i E5q D5i D5i | D5q G5q E5q D5q | D5i D5i D5i D5i D5q C5q | G4h E4q F4q");
@@ -61,8 +79,23 @@ player.play("T140 G4h C5q B4q | A4q A4q G4q F4q | E4q E4i E4i E4q E4q | D4h E4q 
 player.play("T140 G4h C5q B4q | A4q A4q G4q F4q | E4i E4i E4i E4i E4q E4q | D4w");
 
 Rain
-player.play("T130 C5q D5q D#5q F5q | G5h D#5h | F5h D5h | C5w");
-player.play("T130 C5q D5q D#5q F5q | G5h D#5h | F5h G#5h | G5h D#5h | C5w");
+Pattern p1 = new Pattern("C5q D5q D#5q F5q | G5h D#5h | F5h D5h | C5w | Rw | Rw | Rw | Rw")
+                .setVoice(0)
+                .setTempo(130)
+                .setInstrument("Piano");
+
+        Pattern p2 = new Pattern("C3q G3q C4q G3q | C4q G3q D#3q G3q")
+                .setVoice(1)
+                .setTempo(130)
+                .setInstrument("Piano")
+                .repeat(4);
+
+        Pattern p3 = new Pattern("C5q D5q D#5q F5q | G5h D#5h | F5h G#5h | G5h D#5h | C5w")
+                .setVoice(0)
+                .setTempo(130)
+                .setInstrument("Piano");
+
+        player.play(p1, p2, p3);
 
 Super Mario Theme
 player.play("T160 E5i E5q E5q C5i E5q | G5h G4h ");
