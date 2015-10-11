9e90521b4904e3b2c0a4570f3a9c14202d193339
Mitch Gundrum <mgundrum@madisoncollege.edu>
2015-10-10 20:58:06 -0500

    Commit 10.8.1
    Song Project
    V1 melody on time without intro

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -22,6 +22,53 @@ public class HelloWorld {
                 .setTempo(125)
                 .setInstrument("Piano");
 
+        Pattern vocals1_1 = new Pattern("A#4h+Rq_B#4i_C#5h_Ri | C#5i B#4i C#5i B#4i C#5i B#4i C#5i C#5i | D#5i B#4i B#4i B#4q A#4i B#4i A#4i | B#4i A#4i B#4i A#4i B#4s A#4s G#4q.")
+                .setVoice(1)
+                .setTempo(125)
+                .setInstrument("Guitar");
+
+        Pattern vocals1_2 = new Pattern("A#4h+Rq_B#4i_C#5h_Ri | C#5i B#4i C#5i B#4i C#5i B#4i C#5i C#5i | D#5i B#4i B#4i B#4q A#4i G#4i B#4i | Ri A#4i B#4i A#4i B#4i A#4i G#4i C#5")
+                .setVoice(1)
+                .setTempo(125)
+                .setInstrument("Guitar");
+
+        Pattern p3 = new Pattern(p1, vocals1_1, p2, vocals1_2);
+
+        Pattern p4 = new Pattern("C#5h+Rq_G#5q E#6h+Rq_G#5i_C#5i | D#6h+Rq_E#5q C#6h+Rq_C#5q")
+                .setVoice(0)
+                .setTempo(125)
+                .setInstrument("Piano");
+
+        Pattern p5 = new Pattern("B4h+Rq_F#5q D#6h+Rq_F#5i_B4i | C#6h+Rq_D#5q B5h+Rq_B4q")
+                .setVoice(0)
+                .setTempo(125)
+                .setInstrument("Piano");
+
+        Pattern vocals2 = new Pattern("C#5h+Rq_D#5i_E#5h_Ri | E#5i D#5i E#5i D#5i E#5i D#5i E#5i E#5i | E#5i F#5i F#5i E#5q E#5i E#5i E#5i | D#5i C#5i D#5i C#5i D#5i C#5i B4i C#5i")
+                .setVoice(1)
+                .setTempo(125)
+                .setInstrument("Guitar");
+
+        Pattern p6 = new Pattern(p4, p5, vocals2);
+
+        player.play(/*p1.repeat(2),*/ p3.repeat(2), p6.repeat(2));
+    }
+}
+
+
+/*
+
+Total Eclipse of the Heart
+Pattern p1 = new Pattern("A#4h+Rq_E#5q C#6h+Rq_E#5i_A#4i | C6h+Rq_D#5q A#5h+Rq_C#5q")
+                .setVoice(0)
+                .setTempo(125)
+                .setInstrument("Piano");
+
+        Pattern p2 = new Pattern("G#4h+Rq_D#5q B#5h+Rq_D#5i_G#4i | A#5h+Rq_C5q G#5h+Rq_B#4q")
+                .setVoice(0)
+                .setTempo(125)
+                .setInstrument("Piano");
+
         Pattern vocals = new Pattern("A#4h+Rq_B#4i_E#5i")
                 .setVoice(1)
                 .setTempo(125)
@@ -42,11 +89,7 @@ public class HelloWorld {
         Pattern p6 = new Pattern(p4, p5);
 
         player.play(p1.repeat(2), p3.repeat(2), p6.repeat(2));
-    }
-}
 
-
-/*
 
 Experimentation - E minor
 Pattern p1 = new Pattern("Eminw | Bminw | Eminw | Aminw").setVoice(0).setInstrument("Piano");
@@ -67,16 +110,14 @@ Pattern p1 = new Pattern("Eminw | Bminw | Eminw | Aminw").setVoice(0).setInstrum
         player.play(rhythm.getPattern().repeat(4), p1.repeat(2), p2);v
 
 Wasn't Me
-player.play("T140 Rq E5i E5i E5i D5i D5i D5i | D5i D5i G5q E5q D5q | D5i D5i D5i D5i D5q C5q | G4w");
-player.play("T140 Rq E5i E5i E5q D5i D5i | D5q G5q E5q D5q | D5i D5i D5i D5i D5q C5q | G4h E4q F4q");
-player.play("T140 G4h C5q B4q | A4q A4q G4q F4q | E4q E4i E4i E4q E4q | D4h E4q F4q");
-player.play("T140 G4h C5q B4q | A4q A4q G4q F4q | E4i E4i E4i E4i E4q E4q | D4w");
-player.play("T140 Rq C5i C5i C5i C5i C5i C5i | D5q E5q Rq C4i C4i | C4q C5i C5i C5q C5i C5i | D5q E5q Rq C4i C4i");
-player.play("T140 Ri C4i E5i E5i E5i D5i D5q | D5q C5q Rq C4i C4i | C4i E5i E5i E5i E5i E5i E5q | G5q G5q ");
-player.play("T140 Rq E5i E5i E5i D5i D5i D5i | D5i D5i G5q E5q D5q | D5i D5i D5i D5i D5q C5q | G4w");
-player.play("T140 Rq E5i E5i E5q D5i D5i | D5q G5q E5q D5q | D5i D5i D5i D5i D5q C5q | G4h E4q F4q");
-player.play("T140 G4h C5q B4q | A4q A4q G4q F4q | E4q E4i E4i E4q E4q | D4h E4q F4q");
-player.play("T140 G4h C5q B4q | A4q A4q G4q F4q | E4i E4i E4i E4i E4q E4q | D4w");
+Pattern p1 = new Pattern("T140 Rq E5i E5i E5i D5i D5i D5i | D5i D5i G5q E5q D5q | D5i D5i D5i D5i D5q C5q | G4w");
+Pattern p2 = new Pattern("T140 Rq E5i E5i E5q D5i D5i | D5q G5q E5q D5q | D5i D5i D5i D5i D5q C5q | G4h E4q F4q");
+Pattern p3 = new Pattern("T140 G4h C5q B4q | A4q A4q G4q F4q | E4q E4i E4i E4q E4q | D4h E4q F4q");
+Pattern p4 = new Pattern("T140 G4h C5q B4q | A4q A4q G4q F4q | E4i E4i E4i E4i E4q E4q | D4w");
+Pattern p5 = new Pattern("T140 Rq C5i C5i C5i C5i C5i C5i | D5q E5q Rq C4i C4i | C4q C5i C5i C5q C5i C5i | D5q E5q Rq C4i C4i");
+Pattern p6 = new Pattern("T140 Ri C4i E5i E5i E5i D5i D5q | D5q C5q Rq C4i C4i | C4i E5i E5i E5i E5i E5i E5q | G5q G5q ");
+
+player.play(p1, p2, p3, p4, p5, p6, p1, p2, p3, p4);
 
 Rain
 Pattern p1 = new Pattern("C5q D5q D#5q F5q | G5h D#5h | F5h D5h | C5w | Rw | Rw | Rw | Rw")
